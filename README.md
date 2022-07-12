dcd6ec003af2e522a59f9b13c01d23e8d17728c5
ycjcl868 <45808948@qq.com>
2022-07-12 09:39:43 +0800

    feat: lexer

---
+++ README.md
@@ -0,0 +1,43 @@
+# Monkey Programming Language & Interpreter
+
+## Features
+
+- C-like syntax
+- variable bindings
+- integers and booleans
+- arithmetic expressions
+- built-in functions
+- first-class and higher-order functions
+- closures
+- a string data structure
+- an array data structure
+- a hash data structure
+
+example
+
+```js
+let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+  x + y;
+}
+
+let result = add(five, ten);
+```
+
+## Implements
+
+Major Parts
+
+- lexer
+- parser
+- ast(Abstract Syntax Tree)
+- internal object system
+- evaluator
+
+### Lexing
+
+### Parsing
+
+### Evaluation
---
+++ lexer/lexer.go
@@ -0,0 +1,59 @@
+package lexer
+
+import "interpreter/token"
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = NewToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = NewToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = NewToken(token.LPAREN, l.ch)
+	case ')':
+		tok = NewToken(token.RPAREN, l.ch)
+	case ',':
+		tok = NewToken(token.COMMA, l.ch)
+	case '+':
+		tok = NewToken(token.PLUS, l.ch)
+	case '{':
+		tok = NewToken(token.LBRACE, l.ch)
+	case '}':
+		tok = NewToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+}
+
+func NewToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
